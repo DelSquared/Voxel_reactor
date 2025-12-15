d10a8737d6ad2263bd94bf918d8c348c5e2f6832
DelSquared <37172200+DelSquared@users.noreply.github.com>
2025-12-15 14:21:00 +0100

    cleaned up file and added infrastructure for future updates

--- nuclear.c
+++ nuclear.c
@@ -4,28 +4,35 @@
 #include <math.h>
 #include <stdbool.h>
 
+#define NUM_INTER 3
 #define kB 0.0000015
 #define mag(x) sqrt(x[0]*x[0] + x[1]*x[1])
+
+int genID() {
+	static int num = 0; 
+	return num++;      
+}
+
+enum inter {
+  SCATTER,
+  ABSORB,
+  FISS
+};
 
 typedef struct _Neutron Neutron;
 struct _Neutron {
 	float X[2]; 	// position 
 	float V[2]; 	// velocity
 	float L;	// to interaction 
+	enum inter s;	// next interaction type
+	int id;		// identifier for the neutron (to enable reuse of destroyed neutrons by renaming)
+	bool alive;	// if false will stop tracking neutron and consider it absorbed
 };
 
 void printN(Neutron n){
 	printf("Neutron:\n");
 	printf("  X: %e, %e\n", n.X[0], n.X[1]);
 	printf("  V: %e, %e\n", n.V[0], n.V[1]);
-}
-
-void printNE(Neutron n[], int LEN_n){
-	printf("Ensemble:\n");
-	for (int i = 0; i < LEN_n; i++){
-		printf("  ");
-		printN(n[i]);
-	}
 }
 
 void move(Neutron* n){
@@ -37,8 +44,9 @@ void move(Neutron* n){
 void sample_inter_dist(Neutron* n, float s[], int LEN_s){
 	for (int i = 0; i < LEN_s; i++){
         	n->L = -log(1-((float) rand()) / ((float) RAND_MAX))/s[i];
+		n->s = i;
         }
-	printf("%e\n",n->L);
+	//printf("%e\n",n->L);
 }
 
 void scatter(Neutron* n, float A, float T){ //elastic scattering
@@ -47,17 +55,12 @@ void scatter(Neutron* n, float A, float T){ //elastic scattering
 	float vt_dir = 2*M_PI*(float)rand()/(float)RAND_MAX;
 	float vt[2] = {vt_mag*cos(vt_dir), vt_mag*sin(vt_dir)};
 	float vcm[2] = {(vn[0] + A*vt[0]) / (A+1), (vn[1] + A*vt[1]) / (A+1)};
-	printf("x %f, y %f\n", n->V[0], n->V[1]);
+	//printf("x %f, y %f\n", n->V[0], n->V[1]);
 	float Vn[2] = {-vn[0] + 2*vcm[0], -vn[1] + 2*vcm[1]};
 	n->V[0] = Vn[0];
 	n->V[1] = Vn[1];
 }
 
-void ensemble_scatter(Neutron* n, int LEN_n, float A, float T){ //ensemble elastic scattering
-	for (int i=0; i<LEN_n; i++){
-		scatter(&n[i], A, T);
-	}
-}
 
 // event:
 // 	absorption:
@@ -69,14 +72,14 @@ void ensemble_scatter(Neutron* n, int LEN_n, float A, float T){ //ensemble elast
 
 // TODO
 // 	radius of nucleus for fast neutrons R ~ 1e-15*A^0.3333
-// 	energy cross-section fit as s ~ pi*(R + h/(mv))^2
+// 	energy cross-section fit as s ~ pi*(R + h/(mv))^2 ~ pi*(1e-15*A^0.3333 + h/(mv))^2
 // 	add target temp dependence as s(T) = s0 * sqrt(T0 / T)
 //	figure out a lookup table of cross-sections
 //	implement fission and absorption
 
 int main() {
 	
-	Neutron n_test = {.X={0, 0}, .V={20, 0}};
+	Neutron n_test = {.X={0, 0}, .V={20, 0}, .alive=true, .id=genID()};
 	
 	printN(n_test);
 	
@@ -85,30 +88,8 @@ int main() {
 	printN(n_test);
 
 	printf("==================\n");
-
-	int L_n = 5;
-
-	Neutron ns[L_n];
-	for (int i = 0; i < L_n; i++){
-		ns[i].X[0] = 0;
-		ns[i].X[1] = 0;
-		ns[i].V[0] = 1;
-		ns[i].V[1] = 1;
-	}
-
-	printNE(ns, L_n);
-
-	ensemble_scatter(ns, L_n, 5, 500);
-
-	printNE(ns, L_n);
-
-	ensemble_scatter(ns, L_n, 5, 500);
-
-	printNE(ns, L_n);
 	
-	printf("==================\n");
-	
-	Neutron n = {.X={0, 0}, .V={0, 2e6}, .L=0};
+	Neutron n = {.X={0, 0}, .V={0, 2e6}, .L=0, .alive=true, .id=genID()};
 	printN(n);
 	FILE *fp = fopen("out.txt", "w");
 	if (!fp) printf("NO FILE");
@@ -128,7 +109,7 @@ int main() {
 
 	printf("==================\n");
 	
-
+	printf("Neutron struct size: %ld\n", sizeof(Neutron));
 
 
 	return 0;
